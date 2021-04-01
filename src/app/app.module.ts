d69befc6ca3e689a28cdd7c24e58819a05e7bb9e
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-03-31 23:05:06 -0500

    added text and image components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TextcomponentComponent } from './textcomponent/textcomponent.component';
+import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextcomponentComponent,
+    ImagecomponentComponent
   ],
   imports: [
     BrowserModule
