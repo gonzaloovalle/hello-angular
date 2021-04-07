c9d1f0648eb503828b892e6770cb1f7a778d3552
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-04-07 00:33:25 -0500

    added form submission component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TextcomponentComponent } from './textcomponent/textcomponent.component';
 import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
 import { EmpInfoComponent } from './emp-info/emp-info.component';
+import { FormcomponentComponent } from './formcomponent/formcomponent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextcomponentComponent,
     ImagecomponentComponent,
-    EmpInfoComponent
+    EmpInfoComponent,
+    FormcomponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
